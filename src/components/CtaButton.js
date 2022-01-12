92815e89ee9aae3838d31453e02d2a4aa3da13e7
Dan Soutar <dan.soutar@littlerocket.io>
2022-01-12 00:16:49 -0500

    fixing warnings - removed a tag

--- src/components/CtaButton.js
+++ src/components/CtaButton.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 export default function CtaButton() {
   return (
-    <a href='#' className='call-to-action__cta-button'>
+    <div className='call-to-action__cta-button'>
       Start your Project With Pact
-    </a>
+    </div>
   );
 }
